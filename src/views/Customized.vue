3ae99c981f6d6618b5b50b3ec0238baa7d38f6be
彭立璇 <kunbu0203@gmail.com>
2020-11-05 00:11:29 +0800

    新增客製化圖片

--- src/views/Customized.vue
+++ src/views/Customized.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   #customized
     h1 customized
+    .customized-flow
+      img(:src="compileFilePath(`customized.jpg`)")
+      img(:src="compileFilePath(`customized_m.jpg`)")
 </template>
 
 <script>
@@ -11,3 +14,22 @@ export default {
   methods: {}
 };
 </script>
+
+<style lang="sass">
+#customized
+  max-width: 1240px
+  margin: auto
+.customized-flow
+  img
+    &:nth-of-type(2)
+      display: none
+  @media only screen and (max-width: 1024px)
+    max-width: 640px
+    margin: auto
+    img
+      width: 100%
+      &:nth-of-type(1)
+        display: none
+      &:nth-of-type(2)
+        display: block
+</style>
