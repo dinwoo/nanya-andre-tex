e20961740c4ad6682c57570cd20716430c1c93dc
din <danielforboy@gmail.com>
2020-11-05 23:39:12 +0800

    feat: footer component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
   #app
     Header
     router-view
+    Footer
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   components: {
     Header,
+    Footer
   },
   data() {
     return {
