dccc897a133ebf4ca4c08ccb8d8af410b07f4fc3
din <danielforboy@gmail.com>
2020-11-09 00:43:28 +0800

    fix: swiper

--- src/components/CustomSwiper.vue
+++ src/components/CustomSwiper.vue
@@ -1,39 +1,52 @@
 <template lang="pug">
   #custom-swiper
-    .main-carousel
-      VueSlickCarousel(
-        ref="c1"
-        :asNavFor="$refs.c2"
-        :focusOnSelect="true"
-        :arrows="false"
-        :autoplay="true"
-        :autoplaySpeed="3000"
+    .title {{title}}
+    .sub-title {{subTitle}}
+    .option-btn(
+      @click="showOption=!showOption"
+    )
+    .carousel-box
+      .option-box(
+        v-if="showOption"
       )
-        .carousel-item(v-for="(pic,index) in pictureLink" :key="index")
-          //- img(:src="compileFilePath(pic)")
-          .pic(
-            :style="`background-image:url('${compileFilePath(pic)}')`"
-          )
-    .dot-carousel
+        slot(name="option")
+          ul
+            li test
+            li test
+      .main-carousel
+        VueSlickCarousel(
+          ref="c1"
+          :asNavFor="$refs.c2"
+          :focusOnSelect="true"
+          :arrows="false"
+          :autoplay="true"
+          :autoplaySpeed="3000"
+        )
+          .carousel-item(v-for="(pic,index) in pictureLink" :key="index")
+            //- img(:src="compileFilePath(pic)")
+            .pic(
+              :style="`background-image:url('${compileFilePath(pic)}')`"
+            )
       .pre-btn(
         @click="$refs.c2.prev()"
       )
+      .dot-carousel
+        VueSlickCarousel(
+          ref="c2"
+          :asNavFor="$refs.c1"
+          :slidesToShow="slidesToShow"
+          :focusOnSelect="true"
+          :arrows="false"
+          :centerMode="true"
+        )
+          .carousel-item(v-for="(pic,index) in pictureLink" :key="index")
+            //- img(:src="compileFilePath(pic)")
+            .pic(
+              :style="`background-image:url('${compileFilePath(pic)}')`"
+            )
       .next-btn(
         @click="$refs.c2.next()"
       )
-      VueSlickCarousel(
-        ref="c2"
-        :asNavFor="$refs.c1"
-        :slidesToShow="slidesToShow"
-        :focusOnSelect="true"
-        :arrows="false"
-        :centerMode="true"
-      )
-        .carousel-item(v-for="(pic,index) in pictureLink" :key="index")
-          //- img(:src="compileFilePath(pic)")
-          .pic(
-            :style="`background-image:url('${compileFilePath(pic)}')`"
-          )
 </template>
 
 <script>
@@ -45,14 +58,14 @@ export default {
   components: {
     VueSlickCarousel
   },
-  props: ["dotNum", "pictureLink"],
+  props: ["title", "subTitle", "dotNum", "pictureLink"],
   data() {
     return {
-      slidesToShow: 0
+      slidesToShow: 0,
+      showOption: false
     };
   },
   mounted() {
-    console.log(this.compileFilePath(this.pictureLink[0]));
     this.slidesToShow = this.dotNum;
   },
   methods: {}
@@ -62,19 +75,73 @@ export default {
 <style lang="sass" scoped>
 @import "../assets/sass/var.sass"
 
-*:focus
-  outline: none
+*
+  // outline: 1px solid #f00
+  &:focus
+    outline: none
 #custom-swiper
   width: 100%
+  max-width: 1280px
+  padding: 0 15px
+  margin: 30px auto
+  box-sizing: border-box
+  .title
+    font-size: 50px
+    font-weight: bold
+    margin-bottom: 20px
+    border-bottom: 5px solid $orange
+    display: inline-block
+  .sub-title
+    font-size: 30px
+    margin-bottom: 20px
+  .option-btn
+    width: 0
+    height: 0
+    border-right: 30px solid transparent
+    border-top: 30px solid $orange
+    border-bottom: 0px solid transparent
+    border-left: 30px solid transparent
+    margin: 0 auto 20px
+    cursor: pointer
+  .carousel-box
+    padding: 25px 5% 0
+    position: relative
+    font-size: 0
+    .option-box
+      width: 100%
+      padding: 40px 20px
+      background-color: rgba(#fff,.55)
+      box-sizing: border-box
+      box-shadow: 0px 16px 35px rgba(#000,.4)
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      margin: auto
+      z-index: 1
+      ul
+        font-size: 0
+        li
+          width: 50%
+          font-size: 24px
+          padding: 20px 0
+          list-style: disc
+          cursor: pointer
+          transition: .2s
+          +dib
+          +hover
+            color: $orange
   .main-carousel
     width: 100%
     margin: auto
     box-sizing: border-box
   .dot-carousel
-    width: calc(100% - 120px)
+    width: 94%
+    padding: 0 15px
     margin: 15px auto
     box-sizing: border-box
     position: relative
+    +dib
   .carousel-item
     width: 100%
     padding: 0 10px
@@ -86,23 +153,82 @@ export default {
       background-size: cover
       background-position: center center
   .pre-btn,.next-btn
-    width: 0px
-    height: 0px
-    position: absolute
-    top: 0
-    bottom: 0
-    margin: auto
+    width: 3%
+    padding-bottom: 6%
+    background-size: contain
+    background-position: center center
+    background-repeat: no-repeat
+    position: relative
     cursor: pointer
+    +dib
   .pre-btn
-    left: -50px
-    border-right: 30px solid $orange
-    border-top: 30px solid transparent
-    border-bottom: 30px solid transparent
-    border-left: 0px solid transparent
+    background-image: url('../assets/images/arrow_pre.png')
   .next-btn
-    right: -5%
-    border-right: 00px solid transparent
-    border-top: 30px solid transparent
-    border-bottom: 30px solid transparent
-    border-left: 30px solid $orange
+    background-image: url('../assets/images/arrow_next.png')
+  @include rwd(960px)
+    .title
+      font-size: 40px
+      // margin-bottom: 20px
+      // border-bottom: 5px solid $orange
+    .sub-title
+      font-size: 25px
+      // margin-bottom: 20px
+    .option-btn
+      border-right: 20px solid transparent
+      border-top: 20px solid $orange
+      border-bottom: 0px solid transparent
+      border-left: 20px solid transparent
+      // margin: 0 auto 20px
+    .carousel-box
+      // padding: 25px 5% 0
+      .option-box
+        // width: 100%
+        padding: 30px 15px
+        // box-shadow: 0px 16px 35px rgba(#000,.4)
+        ul
+          li
+            // width: 50%
+            font-size: 20px
+            padding: 10px 0
+    .main-carousel
+    .dot-carousel
+      // padding: 0 15px
+      // margin: 15px auto
+    .carousel-item
+      // padding: 0 10px
+      .pic
+    .pre-btn,.next-btn
+  @include rwd(540px)
+    .title
+      font-size: 25px
+      margin-bottom: 10px
+      border-bottom: 2px solid $orange
+    .sub-title
+      font-size: 15px
+      margin-bottom: 10px
+    .option-btn
+      border-right: 10px solid transparent
+      border-top: 10px solid $orange
+      border-bottom: 0px solid transparent
+      border-left: 10px solid transparent
+      margin: 0 auto
+    .carousel-box
+      padding: 15px 5% 0
+      .option-box
+        // width: 100%
+        padding: 10px 15px
+        // box-shadow: 0px 16px 35px rgba(#000,.4)
+        ul
+          li
+            // width: 50%
+            font-size: 16px
+            padding: 5px 0
+    .main-carousel
+    .dot-carousel
+      // padding: 0 15px
+      // margin: 15px auto
+    .carousel-item
+      padding: 0 2px
+      .pic
+    .pre-btn,.next-btn
 </style>
