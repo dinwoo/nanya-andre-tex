d6ad787d50a2edaac23ace63085f5f73a31f8fd7
Daniel Wu <daniel.wu@language-center.com.tw>
2022-02-17 10:28:40 +0800

    fix: 調整頁面切換至頂

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import VueMeta from 'vue-meta';
+import VueMeta from "vue-meta";
 
-import { i18n } from './plugins/i18n'
+import { i18n } from "./plugins/i18n";
 import mixinMethod from "@/mixins/index.js";
 
 import VueImageView from "vue-directive-image-previewer";
@@ -15,17 +15,16 @@ Vue.mixin(mixinMethod);
 Vue.use(VueMeta);
 
 router.afterEach(() => {
-  window.scrollTo(0,300); //切换路由之后滚动条始终在最顶部
+  window.scrollTo(0, 0); //切换路由之后滚动条始终在最顶部
 });
 
 // 點擊圖片放大
 Vue.use(VueImageView, {
   animate: {
-    duration: 300
+    duration: 300,
   },
-  zIndex: "1000"
-})
-
+  zIndex: "1000",
+});
 
 Vue.config.productionTip = false;
 
@@ -33,5 +32,5 @@ new Vue({
   router,
   store,
   i18n,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
